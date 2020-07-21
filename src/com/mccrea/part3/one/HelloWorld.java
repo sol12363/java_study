d9dda3c88d6abe9e63a4076e2086d10d9f9830f6
sol12363 <792619817@qq.com>
2020-07-21 11:11:28 +0800

    3.1练习1 使用正常的打印语句编写一个程序

---
+++ src/com/mccrea/part3/one/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.mccrea.part3.one;
+
+/**
+ * 
+ * @ClassName:  HelloWorld   
+ * @Description: 使用正常的打印语句编写一个程序  
+ * @author: gaojian
+ * @date:   2020年7月21日 上午11:09:19      
+ * @Copyright:
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 * @Title: main   
+	 * @Description: 打印出    Hello World!
+	 * @param: @param args      
+	 * @return: void      
+	 * @throws
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
